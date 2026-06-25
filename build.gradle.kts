a2a500f14df298de0dbb267f42a698917cfae425
ClovenBugle <yan_hun233@outlook.com>
2026-06-26 00:15:29 +0800

    chore(init): initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    `java-library`
+}
+
+group = "top.chiloven"
+version = "0.1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+    withSourcesJar()
+    withJavadocJar()
+}
+
+dependencies {
+    api(libs.jackson.core)
+    api(libs.jackson.databind)
+    compileOnlyApi(libs.jspecify)
+
+    testImplementation(platform(libs.junit.bom))
+    testImplementation(libs.junit.jupiter)
+    testImplementation(libs.assertj.core)
+    testRuntimeOnly(libs.junit.platform.launcher)
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+    options.release.set(21)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
